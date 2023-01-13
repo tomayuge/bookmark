78452049575ec23401401913440274a2cc917ed9
細見真帆 <m.hosomi017@gmail.com>
2023-01-13 16:38:34 +0900

    アカウントのシーダーファイルを修正しました

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Account extends Model
 {
     use HasFactory;
+
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
+    }
 }
