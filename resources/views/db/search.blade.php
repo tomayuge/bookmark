6d3081bc7555769ecc4d124c29e5492b41002a90
妻谷幸司 <t.bookma@gmail.com>
2023-01-13 13:36:09 +0900

    t2

---
+++ resources/views/db/search.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.base')
+@section('title','search')
+@section('main')
+    <h1>検索結果</h1>
+    <a href="/">Topページに戻る</a>
+    <br>
+@endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,5 +8,7 @@
 </head>
 <body>
     <p>aall</p>
+
+    
 </body>
 </html>
