06040a7525c7d0d97374f170b0e7615ee0280f4e
細見真帆 <m.hosomi017@gmail.com>
2023-01-16 16:03:14 +0900

    細見、10日編集分コミットします

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.base')
 @section('title','Bookmark')
 @section('main')
-    <h1>トップページ</h1>
+    
+<!-- フッター -->
+   <footer class="text-center bg-dark text-white">
+        <p class="py-3">bookmark</p>
+    </footer>
+    
+<h1>トップページ</h1>
     <form action="/db/search" method="post">
         @csrf
         <input type="text" name="keyword" required>
-        <input type="submit" value="検索">
+        <input type="submit" value="検索" class="btn btn-outline-dark">
+        <br>
     </form>
     <ul>
-        <li><a href="/db/insert">書籍の新規登録</a></li>
-        <li><a href="/db/list">全件表示</a></li>
+        <!-- <li><a href="/db/insert" >書籍の新規登録</a></li>
+        <li><a href="/db/list">全件表示</a></li> -->
+
+        <a href="/db/insert" class="btn btn-light" >書籍の新規登録</a>
+        <br><br>
+        <a href="/db/list" class="btn btn-light">全件表示</a>
     </ul>
 @endsection
