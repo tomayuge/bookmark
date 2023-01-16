9be4f2960448f76af37ff0a4360967808a4614fb
赤嶺亜衣 <a.akamine828@gmail.com>
2023-01-16 13:46:31 +0900

    store更新

---
+++ resources/views/db/store.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+@section('title','Bookmark')
+@section('main')
+<h2>データを登録しました</h2>
+    <br>
+    <a href="/db/bookView">書籍のページを見る</a>
+    <a href="/db/insert">続けて新規登録する</a>
+    <a href="/">Topページに戻る</a>
+@endsection
