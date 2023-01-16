2b7938e1cc84e92a12bd773c8659493bfa63b04d
細見真帆 <m.hosomi017@gmail.com>
2023-01-16 10:34:54 +0900

    welcome.blade.php と web.phpを編集しました

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return view('index');
 });
 
 Route::post('/db/search','DbController@search');
