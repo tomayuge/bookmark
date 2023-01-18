f7cf06304f56032091212e72b70047a3005c1aa7
赤嶺亜衣 <a.akamine828@gmail.com>
2023-01-18 11:04:51 +0900

    insert

--- resources/views/db/insert.blade.php
+++ resources/views/db/insert.blade.php
@@ -2,9 +2,12 @@
 @section('title','Bookmark')
 @section('main')
 <h2>書籍の新規登録</h2><br>
-    <form action="/db/confirm" method="post">
+    <form action="/db/confirm" method="get">
         @csrf
-        <div>
+        ISBN<input type="text" name="isbnSearch" required>
+        <input type="submit" value="検索">
+
+        <!-- <div>
             <label for="isbn" class="form_label">ISBN</label>
             <input type="text" class="form-control" id="isbn" name="isbn" required><br>
         </div>
@@ -29,7 +32,8 @@
             <input type="text" class="form-control" id="img" name="img" required><br>
         </div><br>
         <input class="btn btn-info w-45" type="submit" value="確認">
-        <br>
+        -->
+    <br>
     <a href="/db/index">Topページに戻る</a>
     </form>
     
