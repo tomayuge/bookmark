968cd1a2bb4be090157669223465b32fd878533c
赤嶺亜衣 <a.akamine828@gmail.com>
2023-01-18 11:05:06 +0900

    web

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::post('/db/login','DbController@login');//新規登録用フォーム(確認)
-Route::get('/index','DbController@index');
+Route::get('/db/index','DbController@index');
 Route::get('/db/insert','DbController@insert');//新規登録用フォーム(ISBN)
 Route::post('/db/confirm','DbController@confirm');//新規登録用フォーム(確認)
 Route::post('/db/store','DbController@store');//新規登録
