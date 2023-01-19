ef129e99df71a86208154dc86172c8ca128cd0bc
赤嶺亜衣 <a.akamine828@gmail.com>
2023-01-19 13:23:24 +0900

    listあ

--- resources/views/db/list.blade.php
+++ resources/views/db/list.blade.php
@@ -26,10 +26,9 @@
                 @csrf
                 <input type="hidden" name="book_id" value="{{ $record -> id }}" readonly>
                 <input type="submit" class="btn btn-link" value="{{ $record -> book_name }}">
-<<<<<<< HEAD
                 </form>
             </td>
-            <td>{{ $record -> reviews -> average('score') }}</td>
+            <td>{{ $record -> reviews -> average('score') }}</td></p>{{ $record -> reviews -> count() }}件</td>
             <td>{{ $record -> writer }}</td>
             <td>{{ $record -> publisher }}</td>
             <td>{{ $record -> ISBN }}</td>
@@ -46,28 +45,13 @@
     </table>
     <br>
     {{ $records->links() }} 
+    <br>
+    <a href="/db/index">Topページに戻る</a>
+    <!-- <a href="/db/index" class="text-dark">Topページに戻る</a> -->
+    <a href="/db/review">レビューページ</a>
+    <!-- <a href="/db/review" class="text-dark">レビューページ</a> -->
     <a href="/db/index">Topページに戻る</a>
     <a href="/db/review">レビューページ</a>
     <script src="https://unpkg.com/vue-star-rating/dist/star-rating.min.js"></script>
-=======
             </form>
-        </td>
-        <td><p>{{ $record -> reviews -> average('score') }}</p>{{ $record -> reviews -> count() }}件</td>
-        <td>{{ $record -> writer }}</td>
-        <td>{{ $record -> publisher }}</td>
-        <td>{{ $record -> ISBN }}</td>
-        <td>￥{{ $record -> price }}</td>
-    </tr>
-    @endforeach
-</table>
-<br>
-{{ $records->links() }}
-<br>
-<a href="/db/index">Topページに戻る</a>
-<!-- <a href="/db/index" class="text-dark">Topページに戻る</a> -->
-<a href="/db/review">レビューページ</a>
-<!-- <a href="/db/review" class="text-dark">レビューページ</a> -->
-
-<script src="https://unpkg.com/vue-star-rating/dist/star-rating.min.js"></script>
->>>>>>> 5662b37d8a4748e6c533b5d7a01fae2027874768
 @endsection
