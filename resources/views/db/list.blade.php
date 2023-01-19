f02d76fa62e9319ed2188c3fee67a6788cfe57bc
妻谷幸司 <t.bookma@gmail.com>
2023-01-19 13:26:14 +0900

    テーブル修正

--- resources/views/db/list.blade.php
+++ resources/views/db/list.blade.php
@@ -10,8 +10,9 @@
     <hr>
 
     <table class="table" border="1">
+    <th><td>書籍名</td><td>レビュー</td><td>著者名</td><td>出版社名</td><td>価格</td></th>
         @foreach($records as $record)
-        <th><td>書籍名</td><td>レビュー</td><td>著者名</td><td>出版社名</td><td>価格</td></th>
+        
         <tr>
             <td>
                 <!-- 画像と本の名前をbookViewへのリンクにしてます -->
@@ -26,30 +27,6 @@
                 @csrf
                 <input type="hidden" name="book_id" value="{{ $record -> id }}" readonly>
                 <input type="submit" class="btn btn-link" value="{{ $record -> book_name }}">
-<<<<<<< HEAD
-                </form>
-            </td>
-            <td>{{ $record -> reviews -> average('score') }}</td>
-            <td>{{ $record -> writer }}</td>
-            <td>{{ $record -> publisher }}</td>
-            <td>{{ $record -> ISBN }}</td>
-            <td>￥{{ $record -> price }}</td>
-            <td>
-                <form action="/db/eraseData" method="post">
-                    @csrf
-                    <input type="hidden" name="book_id" value="{{ $record -> id }}" readonly>
-                    <input type="submit" value="削除">
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-    <br>
-    {{ $records->links() }} 
-    <a href="/db/index">Topページに戻る</a>
-    <a href="/db/review">レビューページ</a>
-    <script src="https://unpkg.com/vue-star-rating/dist/star-rating.min.js"></script>
-=======
             </form>
         </td>
         <td>{{ $record -> reviews -> average('score') }} ({{ $record -> reviews -> count() }}件)</td>
@@ -68,5 +45,4 @@
 <!-- <a href="/db/review" class="text-dark">レビューページ</a> -->
 
 <script src="https://unpkg.com/vue-star-rating/dist/star-rating.min.js"></script>
->>>>>>> 5662b37d8a4748e6c533b5d7a01fae2027874768
 @endsection
