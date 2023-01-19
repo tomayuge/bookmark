9b07567c946bb16544de1554f77cda583a41909f
細見真帆 <m.hosomi017@gmail.com>
2023-01-19 16:01:50 +0900

    bookViewのレイアウト変更、ほそみ

--- resources/views/db/bookView.blade.php
+++ resources/views/db/bookView.blade.php
@@ -14,7 +14,7 @@
 
     <br>
     <br>
-    
+
     <table>
         <tr>
             <td rowspan="6"><img src="{{ $records -> img }}" height="400"></td>
@@ -54,82 +54,84 @@
             <td>{{ $review -> account -> user_name }}</td>
             <td>{{ $review -> score }}</td>
             <td>{{ $review -> comment }}</td>
-            
+
             <td>
                 <!-- モーダルを開くボタン -->
-            <div class="container">
-                <div class="row my-3">
-                
-                    <div class="row mb-5">
-                        <div class="col-2">
-                            <button type="button" class="btn btn-info mb-12 rounded-0" data-toggle="modal" data-target="#editModal" data-backdrop="false">EDIT</button>
-                        </div></td>
+                <div class="container">
+                    <div class="row my-3">
 
-                        <td><div class="col-2">
-                            <button type="button" class="btn btn-dark mb-12 rounded-0" data-toggle="modal" data-target="#eraseModal" data-backdrop="false">ERASE</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- ボタンクリック後に表示される画面の内容 編集用モダール -->
-            <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-                 <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <!-- ここからformタグ -->
-                        <form action="/db/editReview" method="post">
-                        @csrf
-                            <h4 class="modal-title" id="myModalLabel">レビュー編集</h4>
-                        </div>
-                        <div class="modal-body">
-                            <input type="hidden" name="id" value="{{ $review -> id }}">
-                            <input type="textarea" name="comment" value="{{ $review -> comment }}">
-                            <p>評価編集
-                                <br> 
-                                <input type="radio" id="score" name="score" value="1" checked>1
-                                <input type="radio" id="score" name="score" value="2" checked>2
-                                <input type="radio" id="score" name="score" value="3" checked>3
-                                <input type="radio" id="score" name="score" value="4" checked>4
-                                <input type="radio" id="score" name="score" value="5" checked>5
-               
-                            </p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">CLOSE</button>
-                            <input type="submit" class="btn btn-success" value="EDIT">
-                        </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- ボタンクリック後に表示される画面の内容 削除用モダール -->
-            <div class="modal fade" id="eraseModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
-                 <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <!-- ここからformタグ -->
-                        <form action="/db/editReview" method="post">
-                        @csrf
-                            <h4　class="modal-title" id="myModalLabel">レビュー削除</h4>
-                        </div>
-                        <div class="modal-body">
-                            <input type="hidden" name="id" value="{{ $review -> id }}">
-                            <p>{{ $review -> comment }}</p>
-                            <p>パスワード
-                                <br><input type="password" id="pass" name="pass" required>
-                            </p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">CLOSE</button>
-                            <input type="submit" class="btn btn-danger" value="ERASE">
-                        </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        <div class="row mb-5">
+                            <div class="col-2">
+                                <button type="button" class="btn btn-info mb-12 rounded-0" data-toggle="modal" data-target="#editModal" data-backdrop="false">EDIT</button>
+                            </div>
             </td>
-        </tr>
-    @endforeach
+
+            <td>
+                <div class="col-2">
+                    <button type="button" class="btn btn-dark mb-12 rounded-0" data-toggle="modal" data-target="#eraseModal" data-backdrop="false">ERASE</button>
+                </div>
+</div>
+</div>
+</div>
+<!-- ボタンクリック後に表示される画面の内容 編集用モダール -->
+<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- ここからformタグ -->
+                <form action="/db/editReview" method="post">
+                    @csrf
+                    <h4 class="modal-title" id="myModalLabel">レビュー編集</h4>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" name="id" value="{{ $review -> id }}">
+                <input type="textarea" name="comment" value="{{ $review -> comment }}">
+                <p>評価編集
+                    <br>
+                    <input type="radio" id="score" name="score" value="1" checked>1
+                    <input type="radio" id="score" name="score" value="2" checked>2
+                    <input type="radio" id="score" name="score" value="3" checked>3
+                    <input type="radio" id="score" name="score" value="4" checked>4
+                    <input type="radio" id="score" name="score" value="5" checked>5
+
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">CLOSE</button>
+                <input type="submit" class="btn btn-success" value="EDIT">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- ボタンクリック後に表示される画面の内容 削除用モダール -->
+<div class="modal fade" id="eraseModal" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <!-- ここからformタグ -->
+                <form action="/db/editReview" method="post">
+                    @csrf
+                    <h4　class="modal-title" id="myModalLabel">レビュー削除</h4>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" name="id" value="{{ $review -> id }}">
+                <p>{{ $review -> comment }}</p>
+                <p>パスワード
+                    <br><input type="password" id="pass" name="pass" required>
+                </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">CLOSE</button>
+                <input type="submit" class="btn btn-danger" value="ERASE">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</td>
+</tr>
+@endforeach
 </table>
 </div>
 
